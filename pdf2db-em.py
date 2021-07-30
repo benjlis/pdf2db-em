3fbd0700ed43b400284edd4ba07b7417cbdf5d0d
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-07-30 17:37:07 -0400

    Renamed query that retrieves all PDFs

--- pdf2db-em.py
+++ pdf2db-em.py
@@ -1,14 +1,15 @@
 import aiosql
 import psycopg2
 import os
+import pdfemail         # right now it's a symbolic link to pdf2mbox
 
 PDFDIR = os.getenv('PDFDIR')
 conn = psycopg2.connect("")
 queries = aiosql.from_path("pdf2db-em.sql", "psycopg2")
 
-pdfs = queries.dc19pdf_list(conn)
+pdfs = queries.get_dc19pdf_list(conn)
 for p in pdfs:
-    print(p)
+    print(p[1])
 
 #
 pdf_list = os.listdir(PDFDIR)
