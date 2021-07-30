d69524923268fb295385ba9d8443a279321b4a7e
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-07-30 17:03:42 -0400

    Added read of PDFDIR environmental variable

--- pdf2db-em.py
+++ pdf2db-em.py
@@ -1,9 +1,15 @@
 import aiosql
 import psycopg2
+import os
 
+PDFDIR = os.getenv('PDFDIR')
 conn = psycopg2.connect("")
 queries = aiosql.from_path("pdf2db-em.sql", "psycopg2")
 
 pdfs = queries.dc19pdf_list(conn)
 for p in pdfs:
     print(p)
+
+#
+pdf_list = os.listdir(PDFDIR)
+print(pdf_list)
